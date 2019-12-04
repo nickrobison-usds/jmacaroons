82a3142138c3ea4e471dd696fba735f4c4702a2d
Nick Robison <nicholas.a.robison@omb.eop.gov>
2019-12-04 09:32:47 -0500

    Initial fuzz targets

--- jmacaroons-fuzz/src/test/java/com/github/nitram509/jmacaroons/FuzzTests.java
+++ jmacaroons-fuzz/src/test/java/com/github/nitram509/jmacaroons/FuzzTests.java
@@ -1,14 +1,21 @@
 package com.github.nitram509.jmacaroons;
 
+import com.github.nitram509.jmacaroons.util.Base64;
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
 import edu.berkeley.cs.jqf.fuzz.JQF;
 import org.junit.runner.RunWith;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(JQF.class)
 public class FuzzTests {
 
     @Fuzz
-    public void fuzz (String data) {
-        MacaroonsDeSerializer.deserialize(data);
+    public void base64RoundTrip (String data) {
+        final char[] b64 = Base64.encodeUrlSafe(data.getBytes(StandardCharsets.UTF_8));
+        final String rounded = new String(Base64.decode(b64), StandardCharsets.UTF_8);
+        assertEquals("Round trip should be equal", data, rounded);
     }
 }
