8bc9b9074a5747efe2762580934eb5e6d52df65e
Nick Robison <nicholas.a.robison@omb.eop.gov>
2021-01-29 10:18:13 -0500

    Fix some Coverity scan warnings.
    Add null checks to Json deserialization.
    Modify unused fields to make them static (not removing for now, because I didn't write the code).
    
    Fixed some raw getBytes() class to use the specified charset.

--- jmacaroons-core/src/main/java/com/github/nitram509/jmacaroons/crypto/neilalexander/jnacl/verify_16.java
+++ jmacaroons-core/src/main/java/com/github/nitram509/jmacaroons/crypto/neilalexander/jnacl/verify_16.java
@@ -1,17 +1,17 @@
 //
 //  Copyright (c) 2011, Neil Alexander T.
 //  All rights reserved.
-// 
+//
 //  Redistribution and use in source and binary forms, with
 //  or without modification, are permitted provided that the following
 //  conditions are met:
-// 
+//
 //  - Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
 //  - Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 //  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 //  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -29,15 +29,15 @@ package com.github.nitram509.jmacaroons.crypto.neilalexander.jnacl;
 
 public class verify_16
 {
-	final int crypto_verify_16_ref_BYTES = 16;
+	final static int CRYPTO_VERIFY_16_REF_BYTES = 16;
 
 	public static int crypto_verify(byte[] x, int xoffset, byte[] y)
 	{
 		int differentbits = 0;
-		
+
 		for (int i = 0; i < 15; i++)
 			differentbits |= ((int)(x[xoffset + i] ^ y[i])) & 0xff;
-		
+
 		return (1 & (((int)differentbits - 1) >>> 8)) - 1;
 	}
 }
